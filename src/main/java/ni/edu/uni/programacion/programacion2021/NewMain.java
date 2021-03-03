6e044e5bb5249aeefd909594c05c774cbe66c20e
yasser.membreno <yass.membreo.gudiel@gmail.com>
2021-03-03 14:27:26 -0600

    init repository

---
+++ src/main/java/ni/edu/uni/programacion/programacion2021/NewMain.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ni.edu.uni.programacion.programacion2021;
+
+/**
+ *
+ * @author yasser.membreno
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
