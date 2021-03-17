76ef2394eb2d22559e0b1375d0d26335330ce655
yassermembreno <yass.membreo.gudiel@gmail.com>
2021-03-17 08:35:15 -0600

    Added TextEditor App

--- src/main/java/ni/edu/uni/programacion/controllers/PnlTemperatureController.java
+++ src/main/java/ni/edu/uni/programacion/controllers/PnlTemperatureController.java
@@ -7,6 +7,8 @@ package ni.edu.uni.programacion.controllers;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import javax.swing.DefaultComboBoxModel;
 import ni.edu.uni.programacion.views.panels.PnlConversionTemplate;
 
@@ -15,52 +17,65 @@ import ni.edu.uni.programacion.views.panels.PnlConversionTemplate;
  * @author Sistemas-05
  */
 public class PnlTemperatureController implements ActionListener {
+
     private PnlConversionTemplate pnlConversionTemplate;
-    private final String TEMPERATURES[] = new String[]{"Celcius","Fahrenheit"};
+    private final String TEMPERATURES[] = new String[]{"Celcius", "Fahrenheit"};
     private DefaultComboBoxModel fromCmbModel;
     private DefaultComboBoxModel toCmbModel;
-    
+
     public PnlTemperatureController(PnlConversionTemplate pnlConversionTemplate) {
         this.pnlConversionTemplate = pnlConversionTemplate;
         initComponent();
     }
 
-    private void initComponent(){
+    private void initComponent() {
         fromCmbModel = new DefaultComboBoxModel(TEMPERATURES);
         toCmbModel = new DefaultComboBoxModel(TEMPERATURES);
         pnlConversionTemplate.getCmbFrom().setModel(fromCmbModel);
         pnlConversionTemplate.getCmbTo().setModel(toCmbModel);
-        
+
         pnlConversionTemplate.getBtnCalc().addActionListener(this);
         pnlConversionTemplate.getBtnNew().addActionListener(this);
-        
+
+        pnlConversionTemplate.getCmbFrom().addItemListener(
+                (ItemEvent e) -> {
+                    conversionEvent();
+                });
+        pnlConversionTemplate.getCmbTo().addItemListener(
+                (ItemEvent e) -> {
+                    conversionEvent();
+                });
+
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getActionCommand().equalsIgnoreCase("Convertir")){
-            int cmbFromIndex = pnlConversionTemplate.getCmbFrom().getSelectedIndex();
-            int cmbToIndex = pnlConversionTemplate.getCmbTo().getSelectedIndex();
-            double value = Double.parseDouble(pnlConversionTemplate
-                                                                .getTxtValue().getText());
-            pnlConversionTemplate.getLblResult()
-                    .setText("Resultado: " + 
-                            convertidorTemperature(value, cmbFromIndex, cmbToIndex));
-            
+        if (e.getActionCommand().equalsIgnoreCase("Convertir")) {
+            conversionEvent();
         }
     }
-    
-    private double convertidorTemperature(double value, int from, int to){
-        switch(from){
+
+    private void conversionEvent() throws NumberFormatException {
+        int cmbFromIndex = pnlConversionTemplate.getCmbFrom().getSelectedIndex();
+        int cmbToIndex = pnlConversionTemplate.getCmbTo().getSelectedIndex();
+        double value = Double.parseDouble(pnlConversionTemplate
+                .getTxtValue().getText());
+        pnlConversionTemplate.getLblResult()
+                .setText("Resultado: "
+                        + convertidorTemperature(value, cmbFromIndex, cmbToIndex));
+    }
+
+    private double convertidorTemperature(double value, int from, int to) {
+        switch (from) {
             case 0:
-                switch(to){
+                switch (to) {
                     case 0:
-                        return value;                        
+                        return value;
                     case 1:
                         return celciusToFahrenheit(value);
                 }
             case 1:
-                switch(to){
+                switch (to) {
                     case 0:
                         return fahrenheitToCelcius(value);
                     case 1:
@@ -69,12 +84,12 @@ public class PnlTemperatureController implements ActionListener {
         }
         return value;
     }
-    
-    private double celciusToFahrenheit(double value){        
-        return (((double)9/5*value) + 32);
+
+    private double celciusToFahrenheit(double value) {
+        return (((double) 9 / 5 * value) + 32);
     }
-    
-    private double fahrenheitToCelcius(double value){
-        return ((double)5/9 *(value - 32));
+
+    private double fahrenheitToCelcius(double value) {
+        return ((double) 5 / 9 * (value - 32));
     }
 }
