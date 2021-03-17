3717fa2f1035c4a0c03f9c7f0c456238163ba076
yassermembreno <yass.membreo.gudiel@gmail.com>
2021-03-17 11:55:32 -0600

    Added text editor app

--- src/main/java/ni/uni/edu/programacion/Controllers/TemperatureController.java
+++ src/main/java/ni/uni/edu/programacion/Controllers/TemperatureController.java
@@ -7,6 +7,8 @@ package ni.uni.edu.programacion.Controllers;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import javax.swing.DefaultComboBoxModel;
 import ni.uni.edu.programacion.views.panels.PnlConversionTemplate;
 
@@ -37,29 +39,41 @@ public class TemperatureController {
                 (ActionEvent e) -> {
                     btnConvActionPerformed(e);
                 });
+        pnlTemperature.getCmbFrom().addItemListener(
+                (ItemEvent e) -> {
+                    conversionEvent();
+                });
+        pnlTemperature.getCmbTo().addItemListener(
+                (ItemEvent e) -> {
+                    conversionEvent();
+                });
     }
 
     private void btnConvActionPerformed(ActionEvent e) {
+        conversionEvent();
+    }
+
+    private void conversionEvent() throws NumberFormatException {
         int indexFrom = pnlTemperature.getCmbFrom().getSelectedIndex();
         int indexTo = pnlTemperature.getCmbTo().getSelectedIndex();
-        
+
         double value = Double.parseDouble(pnlTemperature.getTxtValue().getText());
-        
+
         double result = convertTemperature(indexFrom, indexTo, value);
         pnlTemperature.getLblResult().setText("Resultado: " + result);
     }
 
-    private double convertTemperature(int from, int to, double value){
-        switch(from){
+    private double convertTemperature(int from, int to, double value) {
+        switch (from) {
             case 0:
-                switch(to){
+                switch (to) {
                     case 0:
                         return value;
                     case 1:
                         return celciusToFahrenheit(value);
                 }
             case 1:
-                switch(to){
+                switch (to) {
                     case 0:
                         return fahrenheitToCelcius(value);
                     case 1:
@@ -68,12 +82,12 @@ public class TemperatureController {
         }
         return value;
     }
-    
-    private double celciusToFahrenheit(double value){
-        return (((double)9/5*value) + 32);
+
+    private double celciusToFahrenheit(double value) {
+        return (((double) 9 / 5 * value) + 32);
     }
-    
-    private double fahrenheitToCelcius(double value){
-        return (((double) 5/9)* (value - 32));
+
+    private double fahrenheitToCelcius(double value) {
+        return (((double) 5 / 9) * (value - 32));
     }
 }
