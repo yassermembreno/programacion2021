b6ed1d341e72d952c880ac024789bff653050005
yassermembreno <yass.membreo.gudiel@gmail.com>
2021-03-17 10:33:43 -0600

    Added text editor app

--- src/main/java/ni/edu/uni/programacion/controllers/PnlTemperatureController.java
+++ src/main/java/ni/edu/uni/programacion/controllers/PnlTemperatureController.java
@@ -7,6 +7,8 @@ package ni.edu.uni.programacion.controllers;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import javax.swing.DefaultComboBoxModel;
 import ni.edu.uni.programacion.views.panels.PnlConversionTemplate;
 
@@ -37,14 +39,27 @@ public class PnlTemperatureController {
                 (ActionEvent e) -> {
                     btnConvActionPerformed(e);
                 });
+
+        pnlConversionTemplate.getCmbFrom().addItemListener(
+                (ItemEvent e) -> {
+                    conversionEvent();
+                });
+        pnlConversionTemplate.getCmbTo().addItemListener(
+                (ItemEvent e) -> {
+                    conversionEvent();
+                });
     }
 
     private void btnConvActionPerformed(ActionEvent e) {
+        conversionEvent();
+    }
+
+    private void conversionEvent() throws NumberFormatException {
         int indexFrom = pnlConversionTemplate.getCmbFrom().getSelectedIndex();
         int indexTo = pnlConversionTemplate.getCmbTo().getSelectedIndex();
         double value = Double.parseDouble(
                 pnlConversionTemplate.getTxtValue().getText());
-        
+
         double result = convertirTemperature(value, indexFrom, indexTo);
         pnlConversionTemplate.getLblResult().setText("Resultado: " + result);
     }
